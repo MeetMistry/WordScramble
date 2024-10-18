f4c5dbe5724d07e7d87d62bedbacf5d9eff8258d
Meet Mistry <meet.m@simformsolutions.com>
2024-10-18 08:15:42 +0530

    Word scramble

---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Meet Mistry on 13/10/24.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
